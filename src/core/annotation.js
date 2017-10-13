f9527f181a83ea064f74888573ad186e52f3624b
Pushkar Anand <pushkar8723@gmail.com>
2017-10-13 09:44:59 +0530

    Showing digital signatures.

--- src/core/annotation.js
+++ src/core/annotation.js
@@ -583,7 +583,7 @@ class WidgetAnnotation extends Annotation {
 
     // Hide signatures because we cannot validate them.
     if (data.fieldType === 'Sig') {
-      this.setFlags(AnnotationFlag.HIDDEN);
+      this.setFlags(AnnotationFlag.READONLY);
     }
   }
 
